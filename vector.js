0aaff73ab807ce9bab81cd5cc57c7d0fce3b91db
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-09-20 09:56:52 +0100

    initial commit

---
+++ vector.js
@@ -0,0 +1,9 @@
+//// Vector Class
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function(vector) {
+  return new Vector(this.x + vector.x, this.y + vector.y);
+};
+
